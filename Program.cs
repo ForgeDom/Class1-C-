bd19bd9be5a754813bb8dbb2a824e1500d0a0c83
Max <maxstill2006@gmail.com>
2024-11-19 16:22:01 +0200

    +

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter line length:");
+            var length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the sign:");
+            var sign = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter 1 - Horizontal line; 2 - Vertical line");
+            var choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if (choice == '1')
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(sign);
+                }
+            }
+            else if(choice == '2')
+            {
+                for(int i = 0;i < length; i++)
+                {
+                    Console.WriteLine(sign);
+                }
+            }
         }
     }
 }
