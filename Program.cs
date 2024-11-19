10a52cd76bfa135065c04c05f7c2f89afa3bd5b4
Max <maxstill2006@gmail.com>
2024-11-19 15:16:18 +0200

    +

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Enter number with 6 units:");
+                number = Convert.ToInt32(Console.ReadLine());
+                string numberStr = number.ToString();
+                if (numberStr.Length == 6)
+                {
+                    String reverse = new string(number.ToString().Reverse().ToArray());
+                    Console.WriteLine($"Reversed number -  {reverse}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Number doesn`t have 6 units!!!");
+                }
+            }
 
         }
     }
