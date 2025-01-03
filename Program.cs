f579443dd9379fdd54c13b4de45e87e3ba41c8b0
ForgeDom <maxstill2006@gmail.com>
2025-01-03 22:21:28 +0200

    +

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-
-        }
+            Console.WriteLine("Enter number:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the % for this number you want to get:");
+            double percentage = Convert.ToDouble(Console.ReadLine());
+            double result = number * percentage / 100;
+            Console.WriteLine(result);
+            }
     }
 }
