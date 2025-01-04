dbadd67e0a920ab5441e246426b8a95026a67c2a
ForgeDom <maxstill2006@gmail.com>
2025-01-04 12:20:39 +0200

    +

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-
+            string sum = "";
+            Console.WriteLine("Enter 4 numbers:");
+            for (int i = 0; i < 4; i++)
+            {
+                string input = Console.ReadLine();
+                sum += input;
+            }
+            Console.WriteLine($"Result - {sum}");
         }
     }
 }
