64d434e248a6fd0373773700f4fdf46291a8a937
ForgeDom <maxstill2006@gmail.com>
2025-01-06 18:50:37 +0200

    +

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace ConsoleApp1_C_
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Globalization;
 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть дату у форматі ДД.ММ.РРРР (наприклад, 22.12.2021):");
+
+        string input = Console.ReadLine();
+
+        if (DateTime.TryParseExact(input, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+        {
+            string season = GetSeason(date);
+
+            string dayOfWeek = date.ToString("dddd", CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{season} {dayOfWeek}");
+        }
+        else
+        {
+            Console.WriteLine("Некоректний формат дати. Спробуйте ще раз.");
+        }
+    }
+
+    static string GetSeason(DateTime date)
+    {
+        int month = date.Month;
+        int day = date.Day;
+
+        if ((month == 12 && day >= 21) || month == 1 || month == 2 || (month == 3 && day < 21))
+        {
+            return "Winter";
+        }
+        else if ((month == 3 && day >= 21) || month == 4 || month == 5 || (month == 6 && day < 21))
+        {
+            return "Spring";
+        }
+        else if ((month == 6 && day >= 21) || month == 7 || month == 8 || (month == 9 && day < 21))
+        {
+            return "Summer";
+        }
+        else
+        {
+            return "Autumn";
         }
     }
 }
